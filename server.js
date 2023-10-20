705c2da43680135d5b1ea36bf65c15fbeeabc263
Taras Kovalyk <tarik2454@gmail.com>
2023-10-20 11:33:15 +0300

    Fix bug render

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
+const mongoose = require('mongoose');
 const app = require('./app');
+// fGzeRwSWmSTggUGk
 
-app.listen(3000, () => {
-  console.log('Server running. Use our API on port: 3000');
-});
+const DB_HOST =
+  'mongodb+srv://tarik2454:fGzeRwSWmSTggUGk@cluster0.qhdukbs.mongodb.net/goit-nodejs-hw-rest-api?retryWrites=true&w=majority';
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log(
+        'Database connection successful. Server running. Use our API on port: 3000'
+      );
+    });
+  })
+  .catch(error => {
+    console.warn(error.message);
+    process.exit(1);
+  });
